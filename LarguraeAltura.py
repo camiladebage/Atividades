5d4fd46cf7c4afbb30c57c175e4398a6e789ea9e
Camila Debage <51386734+camiladebage@users.noreply.github.com>
2021-12-28 13:45:24 -0300

    Create LarguraeAltura.py

---
+++ LarguraeAltura.py
@@ -0,0 +1,7 @@
+largura = float(input('Digite a largura da parede: '))
+altura = float(input('Digite a altura da parede: '))
+
+area = altura * largura
+
+print(f'A área da sua parede é: {area} ')
+print(f'Você terá que usar {area / 2}')
