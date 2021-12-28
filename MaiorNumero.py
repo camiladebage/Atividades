185205b0b756f1afc928332ddc956ddd5470fe30
Camila Debage <51386734+camiladebage@users.noreply.github.com>
2021-12-28 13:49:44 -0300

    Create MaiorNumero.py

---
+++ MaiorNumero.py
@@ -0,0 +1,9 @@
+num1 = input('Digite o número: ')
+num2 = input('Digite o número: ')
+
+if (num1 > num2):
+  print('O maior número é: ' + num1)
+elif (num2 > num1) :
+  print('O maior número é: ' + num2) 
+else :
+  print('Os dois números são iguais') 
